c4d6eb7deba9bb54277ef4f2b78fde74ba98682b
Burnninel <79659103+Burnninel@users.noreply.github.com>
2021-12-02 18:58:46 -0300

    introdução de images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Botao from "../components/Botao";
 import Header from "../components/Header";
-import userInsta from "../components/userInsta";
+import img1 from "../../public/image/tanjiro.png"
+import img2 from "../../public/image/itadori.png"
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,32 +10,25 @@ export default function Home() {
       <div>
         <Header />
       </div>
-      <div
-        className={`
-        h-screen 
-        flex justify-center flex-col items-center 
-      `}
-      >
-        <div className="mt-24">
-          <h1
-            className={`
-          tracking-normal text-sm
-          flex justify-center items-center
-          font-mono uppercase  text-white
-        `}
-          >
-            Clique em uma das opções abaixo para ser direcionado a pagina:
-          </h1>
+      <div className={`h-screen flex justify-center items-center`}>
+        <div className="w-1/5 mt-32">
+          <Image src={img1} />
         </div>
-        <div className={`mt-6 `}>
+        <div className={`flex flex-col items-center w-2/5 m-16 mb-1`}>
+          <h1 className={`tracking-normal text-sm flex justify-center items-center font-mono uppercase  text-white`}>
+                Clique em uma das opções abaixo para ser direcionado a pagina:
+          </h1>
           <Botao nome="Cursos" url="https://www.google.com/" />
           <Botao nome="Blog Otome" url="https://www.google.com/" />
           <Botao nome="Minha Loja" url="https://www.google.com/" />
         </div>
+        <div className="w-1/5 mt-32">
+            <Image src={img2}/>
+        </div>
       </div>
       <div className={`
         flex flex-col items-center mb-16
-        font-normal uppercase text-white
+        text-sm uppercase text-white
       `}>
           <span>@otomebrasil_</span>
           <span>@itachi.do.genjutsu</span>
